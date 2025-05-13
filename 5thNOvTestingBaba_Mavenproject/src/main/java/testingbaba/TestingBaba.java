f453b17738af8147bcecc589628549750370bd2e
samsingh9 <167865695+samsingh9@users.noreply.github.com>
2025-05-13 12:36:23 +0530

    Add files via upload

---
+++ 5thNOvTestingBaba_Mavenproject/src/main/java/testingbaba/TestingBaba.java
@@ -0,0 +1,16 @@
+package testingbaba;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
+public class TestingBaba {
+
+   @Test
+	public void launchUrl()
+	{
+		
+	WebDriver driver = new ChromeDriver();
+	}
+}
